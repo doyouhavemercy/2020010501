6ac1a35a80253ce2a9a39f488e51f9fc1556d973
doyouhavemercy <144317822+doyouhavemercy@users.noreply.github.com>
2023-12-21 14:20:55 +0900

    기말고사 프로젝트
    기말고사 프로젝트입니다. 주제는 요즘 대학생이라면 없을 수 없는 인스타그램 관련주제이며 사람들의 아이디를 저장하고 지울 수 있는 기능을 가진 전화기록부와 유사한 프로그램입니다.

--- main.cpp
+++ main.cpp
@@ -1,67 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#define _CRT_SECURE_NO_WARNINGS_
+#include <string.h>
 
-int main(void)
-{
-	int num;
-	int *ap;
-	int  i, j, res = 0;
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_IG_LENGTH 30
+#define MAX_CONTACTS 10
 
-	printf(">양수 입력 : ");
-	scanf_s("%d", &num);
-	ap = (int*)malloc((num-1) * sizeof(int));
+typedef struct {
+    char name[MAX_CONTACTS][MAX_IG_LENGTH + 1];
+    char id[MAX_CONTACTS][MAX_IG_LENGTH + 1];
+    size_t size;
+} WhitePages;
 
-	if (ap == NULL)
-	{
-		printf("메모리가 부족합니다!\n");
-		exit(1);
-	}
-	for (i = 0; i < (num - 1); i++)
-	{
-		ap[i] = i + 2;
-	}
-	i = 0;
+void saveNameIG(WhitePages* whitePages);
+void findID(const WhitePages* whitePages);
+void findName(const WhitePages* whitePages);
+void removeContact(WhitePages* whitePages);
 
-	while (1)
-	{
-		while ((ap[i] == 0) && (i < (num - 2)))
-		{
-			i++;
-		}
-		
-		if (i == (num - 2))
-			break;
-		
-		for (j = i + 1; j < (num - 1); j++)
-		{
-			if ((ap[j] % ap[i]) == 0)
-			{
-				ap[j] = 0;
-			}
-		}
-		i++;
-	}
+int main(void) {
+    WhitePages whitePages = { {""}, {""}, 0 };
+    int nums;
+    int i;
 
-	for (i = 0; i < (num - 1); i++)
-	{
-		if (ap[i] != 0) //입력값 내의 숫자가 소수일시 출력
-		{
-			printf("%3d", ap[i]);
-		}
+    do {
+    printf("Select code.\n");
+    printf("1: Save ID and name.\n");
+    printf("2: Find ID.\n");
+    printf("3: Find name.\n");
+    printf("4: Remove contact\n");
+    scanf("%d", &nums);
 
-		else // 입력값 내의 숫자가 약분이 가능한 숫자일 경우 %c로 이용한 X로 표시
-		{
-			printf("%3c", 'X');
-		}
-		if ((i + 1) % 5 == 0)
-		{
-			printf("\n");
-		}
 
-	}
-	free(ap);
+        switch (nums) {
+        case 1:
+            saveNameIG(&whitePages);
+            break;
+        case 2:
+            findID(&whitePages);
+            break;
+        case 3:
+            findName(&whitePages);
+            break;
+        case 4:
+            removeContact(&whitePages);
+            break;
+        default:
+            printf("Error: Invalid code!\n");
+            break;
+        }
+    } while (i = !0);
+    return 0;
+}
 
-	return 0;
+void saveNameIG(WhitePages* whitePages) {
+    if (whitePages->size < MAX_CONTACTS) {
+        printf("Insert ID and name.\n");
+        printf("ID: ");
+        scanf("%s", whitePages->id[whitePages->size]);
+
+        printf("Name: ");
+        scanf("%s", whitePages->name[whitePages->size]);
+
+        whitePages->size++;
+        printf("Contact saved successfully!\n");
+    }
+    else {
+        printf("Error: Contact list is full.\n");
+    }
+}
+
+void findID(const WhitePages* whitePages) {
+    char searchName[MAX_IG_LENGTH + 1];
+    printf("Enter the name to find: ");
+    scanf("%s", searchName);
+
+    for (size_t i = 0; i < whitePages->size; ++i) {
+        if (strcmp(whitePages->name[i], searchName) == 0) {
+            printf("ID found for %s: %s\n", searchName, whitePages->id[i]);
+            return;
+        }
+    }
+
+    printf("Contact not found for the given name.\n");
+}
+
+void findName(const WhitePages* whitePages) {
+    char searchID[MAX_IG_LENGTH + 1];
+    printf("Enter the ID to find: ");
+    scanf("%s", searchID);
+
+    for (size_t i = 0; i < whitePages->size; ++i) {
+        if (strcmp(whitePages->id[i], searchID) == 0) {
+            printf("Name found for ID %s: %s\n", searchID, whitePages->name[i]);
+            return;
+        }
+    }
+
+    printf("Contact not found for the given ID.\n");
+}
+
+void removeContact(WhitePages* whitePages) {
+    printf("Choose contact to remove:\n");
+
+    for (size_t j = 0; j < whitePages->size; ++j) {
+        printf("%zu - Name: %s\tID: %s\n", j + 1, whitePages->name[j], whitePages->id[j]);
+    }
+
+    size_t choice;
+    printf("Enter the number of the contact to remove: ");
+    scanf("%zu", &choice);
+
+    if (choice > 0 && choice <= whitePages->size) {
+        // Use fflush to clear the input buffer
+        fflush(stdin);
+
+        // Use gets to read the newline character left in the buffer
+        gets_s(whitePages->name[choice - 1]);
+        gets_s(whitePages->id[choice - 1]);
+
+        // Shift elements to remove the chosen contact
+        for (size_t i = choice - 1; i < whitePages->size - 1; ++i) {
+            strcpy(whitePages->name[i], whitePages->name[i + 1]);
+            strcpy(whitePages->id[i], whitePages->id[i + 1]);
+        }
+
+        whitePages->size--;
+        printf("Contact removed successfully.\n");
+    }
+    else {
+        printf("Error: Invalid contact number.\n");
+    }
 }
